6c504c8e0a1b133a2100940027a4c75fefe4b610
Kyle Felter <kfelter@nvidia.com>
2026-04-30 16:11:33 -0500

    Revert "fix: Use SPDX-only source headers"
    This reverts commit 9dc9dc43113e4a0ab8ad126b8a13a52e208dde0e.

--- rla/internal/carbideapi/tweak_protodefs.sh
+++ rla/internal/carbideapi/tweak_protodefs.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+
 # SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 # This script tweaks the original protodefs from carbide because they're messy in a way that keeps us from building.
 # Try and keep this script safe to rerun on the protodefs multiple times.
@@ -53,6 +67,18 @@ sedi -e 's/MachineValidationStarted started/MachineValidationStarted oneof_start
 # Prepend SPDX license header to all proto files so protoc-gen-go carries it into generated .pb.go
 LICENSE_HEADER="// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the \"License\");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an \"AS IS\" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 "
 for f in carbideproto/*.proto; do
     if ! head -1 "$f" | grep -q "SPDX"; then
